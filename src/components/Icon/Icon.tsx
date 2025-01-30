3fa3087ea2e3a36e105449760c4108bff692e69f
Shaun Fox <shaunrfox@gmail.com>
2025-01-30 10:57:50 -0600

    Refactored Icon to use Box

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,35 +2,25 @@ import { cx } from '@styled-system/css';
 import { iconStyle } from './iconStyle';
 import { IconNamesList } from './icons';
 import { type HTMLStyledProps } from '@styled-system/types';
+import { Box } from '../Box/Box';
 
 type IconProps = HTMLStyledProps<'svg'> & {
   name: IconNamesList;
   className?: string;
 };
 
-const IconSVG = ({
-  children,
-  className,
-  ...props
-}: HTMLStyledProps<'svg'> & {
-  children: React.ReactNode;
-}) => {
+export function Icon({ name, className, ...props }: IconProps) {
   return (
-    <svg
+    <Box
+      as="svg"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       className={cx(iconStyle(), className)}
+      width="24"
+      height="24"
       {...props}
     >
-      {children}
-    </svg>
-  );
-};
-
-export const Icon = ({ name, className, ...props }: IconProps) => {
-  return (
-    <IconSVG className={className} width="24" height="24" {...props}>
       <use xlinkHref={`/sprite.svg#${name}`} />
-    </IconSVG>
+    </Box>
   );
-};
+}
