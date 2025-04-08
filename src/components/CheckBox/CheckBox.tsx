87b4a31bf71a6d430633b2d55858b18f37750431
Yash-CETEC <yash@tech-x.com>
2025-04-08 15:54:10 +0530

    fix: typescript and eslint error resolved

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,37 +1,30 @@
-import { Box, type BoxProps } from "../Box";
+import { Box, type BoxProps } from '../Box';
 import { ReactNode } from 'react';
-import { Label } from "../Label";
-import { checkbox, type CheckboxVariantProps } from "@styled-system/recipes";
-import { Icon } from "../Icon";
+import { Label } from '../Label';
+import { checkbox, type CheckboxVariantProps } from '@styled-system/recipes';
+import { Icon } from '../Icon';
 
-export type CheckBoxProps = Omit<BoxProps, keyof CheckboxVariantProps > & CheckboxVariantProps & 
-    {
-        indeterminate?: boolean;
-        error?: boolean;
-        children?: Omit<ReactNode, keyof HTMLInputElement> | string;
-    };
+export type CheckBoxProps = Omit<BoxProps, keyof CheckboxVariantProps> &
+  CheckboxVariantProps & {
+    indeterminate?: boolean;
+    error?: boolean;
+    children?: ReactNode | string;
+  };
 
-export const CheckBox: React.FC<CheckBoxProps> = (
-  {
-    error = false,
-    indeterminate = false,
-    children,
-    ...props
-}: CheckBoxProps, 
-) => {
-    const { container, input, indicator } = checkbox({});
-    return (
-        <Label className={container}>
-            <Box
-                as="input"
-                type="checkbox"
-                className={input}
-                {...props}
-            />
-            <Icon className={indicator} name={'checkbox'} />
-            <Icon className={indicator} name={'checkbox-checked'} />
-            <Icon className={indicator} name={'checkbox-indeterminate'} />
-            {children}
-        </Label>
-    );
-}
+export const CheckBox: React.FC<CheckBoxProps> = ({
+  error = false,
+  indeterminate = false,
+  children,
+  ...props
+}: CheckBoxProps) => {
+  const { container, input, indicator } = checkbox({});
+  return (
+    <Label className={container}>
+      <Box as="input" type="checkbox" className={input} {...props} />
+      <Icon className={indicator} name={'checkbox'} />
+      <Icon className={indicator} name={'checkbox-checked'} />
+      <Icon className={indicator} name={'checkbox-indeterminate'} />
+      {children}
+    </Label>
+  );
+};
