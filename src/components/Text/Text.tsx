0e6c99e700455269fbba7f7638eb0d1e0ad2b66b
Yash-CETEC <yash@tech-x.com>
2025-05-02 19:05:26 +0530

    fix: optimized text component to remove rendunt props

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,19 +1,11 @@
 import React, { ReactNode, type ElementType } from 'react';
 import { Box, type BoxProps } from '~/components/Box';
-import type { FontToken, FontWeightToken } from '@styled-system/tokens';
 import { text, type TextVariantProps } from '@styled-system/recipes';
 import { cx } from '@styled-system/css';
 import { splitProps } from '~/utils/splitProps';
-import { TextSizes } from '~/recipes/text';
 
 export type TextProps = Omit<BoxProps, keyof TextVariantProps> &
   TextVariantProps & {
-    italic?: boolean;
-    family?: FontToken;
-    bold?: boolean;
-    underline?: boolean;
-    size?: TextSizes;
-    weight?: FontWeightToken;
     children?: string | ReactNode;
     as?: ElementType;
   };
@@ -25,7 +17,6 @@ export const Text: React.FC<TextProps> = ({
   bold = false,
   underline = false,
   size,
-  weight,
   children,
   ...props
 }: TextProps) => {
