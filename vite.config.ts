753c6d6ba69a90b70e32f4833d205ff4faf2c3c0
Ethan Wiggins <ethanwiggins@Ethans-MacBook-Air.local>
2025-06-04 17:37:10 -0500

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: [
       { find: '~', replacement: path.resolve(__dirname, './src') },
@@ -20,22 +18,22 @@ export default defineConfig({
   ...(process.env.GH_REPO
     ? {}
     : {
-      build: {
-        lib: {
-          name: 'cetecComponents',
-          fileName: (format) => `cetec-components.${format}.js`,
-        entry: './src/main.tsx',
-          formats: ['es'],
-        },
-        rollupOptions: {
-          external: ['react', 'react-dom', 'react/jsx-runtime'],
-          output: {
-            globals: {
-              react: 'React',
-              'react-dom': 'ReactDOM',
+        build: {
+          lib: {
+            name: 'cetecComponents',
+            fileName: (format) => `cetec-components.${format}.js`,
+            entry: './src/main.tsx',
+            formats: ['es'],
+          },
+          rollupOptions: {
+            external: ['react', 'react-dom', 'react/jsx-runtime'],
+            output: {
+              globals: {
+                react: 'React',
+                'react-dom': 'ReactDOM',
+              },
             },
           },
         },
-      },
-    }),
+      }),
 });
