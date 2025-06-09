ac10a3aacf48f7e11d6babef98fccca4b1cb5e90
Ethan Wiggins <ethanwiggins@Ethans-MacBook-Air.local>
2025-06-09 15:04:46 -0500

    vite fixes

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,6 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'cetec-design-system',
-      formats: ['es'],
-    },
-    rollupOptions: {
-      preserveEntrySignatures: 'exports-only',
-    },
-  },
   plugins: [react()],
   resolve: {
     alias: {
@@ -41,7 +31,7 @@ export default defineConfig({
           lib: {
             name: 'Cetec Design System',
             fileName: (format) => `cetec-design-system.${format}.js`,
-            entry: './src/main.tsx',
+            entry: resolve(__dirname, 'src/index.ts'),
             formats: ['es'],
           },
           rollupOptions: {
