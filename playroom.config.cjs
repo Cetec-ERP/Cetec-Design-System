da87cf71f41fc73618b42a2a9003908d023ed23f
David Atkins <david.atkins@me.com>
2025-07-18 11:08:46 -0500

    pausing work here, going to backtrack and try a different plan

--- playroom.config.cjs
+++ playroom.config.cjs
@@ -89,6 +89,7 @@ module.exports = {
                                         'nesting-rules': true,
                                     },
                                 }),
+                                '@csstools/postcss-cascade-layers',
                             ],
                             },
                         },
