055066644b941f68dc478e8bdde92679a2b54b7f
Smit Patel <smitpatel@Smits-MacBook-Air.local>
2025-08-14 12:54:32 +0530

    align issue resolved

--- src/recipes/breadcrumbs.ts
+++ src/recipes/breadcrumbs.ts
@@ -1,22 +1,26 @@
-import { defineRecipe } from "@pandacss/dev";
+import { defineRecipe } from '@pandacss/dev';
 
 const BreadcurmbsBase = {
+  display: 'flex',
+  alignItems: 'center',
+  '& li': {
     display: 'flex',
     alignItems: 'center',
-    "& a":{
-        color: {base:'slate.60', _dark: 'slate.60'},
-    },
-    "& p":{
-        color: {base: 'slate.90', _dark: 'slate.0'},
-    },
-    "& span":{
-        mx: '6',
-        color: 'slate.20'
-    }
-}
+  },
+  '& a': {
+    color: { base: 'slate.60', _dark: 'slate.60' },
+  },
+  '& p': {
+    color: { base: 'slate.90', _dark: 'slate.0' },
+  },
+  '& span': {
+    mx: '6',
+    color: 'slate.20',
+  },
+};
 
 export const breadcurmbsRecipe = defineRecipe({
-    className:"breadcurmbs",
-    jsx:['Breadcurmbs'],
-    base: BreadcurmbsBase,
-})
+  className: 'breadcurmbs',
+  jsx: ['Breadcurmbs'],
+  base: BreadcurmbsBase,
+});
