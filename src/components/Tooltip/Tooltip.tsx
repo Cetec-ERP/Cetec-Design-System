1a10909c9ec16a747b2108690e1f05a69dd9c332
Smit Patel <smitpatel@Smits-MacBook-Air.local>
2025-08-28 11:55:41 +0530

    third time merged

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -13,6 +13,21 @@ export type TooltipProps = Omit<BoxProps, keyof TooltipVariantProps> &
     trigger?: 'onHover' | 'onClick';
   };
 
+const allPlacements: TooltipVariantProps['placement'][] = [
+  'top',
+  'right',
+  'bottom',
+  'left',
+  'top-start',
+  'top-end',
+  'right-start',
+  'right-end',
+  'bottom-end',
+  'bottom-start',
+  'left-start',
+  'left-end',
+];
+
 export const Tooltip: FC<TooltipProps> = ({
   trigger = 'onHover',
   caret = true,
@@ -22,9 +37,159 @@ export const Tooltip: FC<TooltipProps> = ({
   placement = 'bottom',
   ...props
 }) => {
-  const { wrapper, tooltipContent } = tooltip({ placement, caret });
+  const [currentPlacement, setCurrentPlacement] = useState(placement);
+  const { wrapper, tooltipContent } = tooltip({
+    placement: currentPlacement,
+    caret,
+  });
   const [show, setShow] = useState(false);
   const tooltipRef = useRef<HTMLDivElement>(null);
+  const triggerRef = useRef<HTMLDivElement>(null);
+  const resolvedPlacement =
+    typeof placement === 'string' ? placement : 'bottom';
+
+  const checkPlacement = () => {
+    const tooltipPostition = tooltipRef.current;
+    const trigger = triggerRef.current;
+    if (!tooltipPostition || !trigger) return;
+
+    const triggerRect = trigger.getBoundingClientRect();
+
+    for (const positioning of [
+      resolvedPlacement,
+      ...allPlacements.filter((pl) => pl !== resolvedPlacement),
+    ]) {
+      const tooltipRect = getSimulatedRect(
+        triggerRect,
+        tooltipPostition.offsetWidth,
+        tooltipPostition.offsetHeight,
+        positioning,
+      );
+      const fits =
+        tooltipRect.top >= 0 &&
+        tooltipRect.left >= 0 &&
+        tooltipRect.bottom <= window.innerHeight &&
+        tooltipRect.right <= window.innerWidth;
+      if (fits) {
+        setCurrentPlacement(positioning);
+        return;
+      }
+    }
+    setCurrentPlacement(placement);
+  };
+
+  type Placement = 'top' | 'bottom' | 'left' | 'right' | 'top-start' | 'bottom-start' | 'left-start' | 'right-start' | 'top-end' | 'bottom-end' | 'left-end' | 'right-end'
+
+  function getSimulatedRect(
+    triggerRect: DOMRect,
+    tooltipWidth: number,
+    tooltipHeight: number,
+    place: Placement,
+  ) {
+    const gap = 8;
+
+    const verticalCenter =
+      triggerRect.left + triggerRect.width / 2 - tooltipWidth / 2;
+    const horizontalCenter =
+      triggerRect.top + triggerRect.height / 2 - tooltipHeight / 2;
+
+    switch (place) {
+      case 'top':
+        return {
+          top: triggerRect.top - tooltipHeight - gap,
+          bottom: triggerRect.top - gap,
+          left: verticalCenter,
+          right: verticalCenter + tooltipWidth,
+        };
+      case 'top-start':
+        return {
+          top: triggerRect.top - tooltipHeight - gap,
+          bottom: triggerRect.top - gap,
+          left: triggerRect.left,
+          right: triggerRect.left + tooltipWidth,
+        };
+      case 'top-end':
+        return {
+          top: triggerRect.top - tooltipHeight - gap,
+          bottom: triggerRect.top - gap,
+          left: triggerRect.right - tooltipWidth,
+          right: triggerRect.right,
+        };
+      case 'bottom':
+        return {
+          top: triggerRect.bottom + gap,
+          bottom: triggerRect.bottom + tooltipHeight + gap,
+          left: verticalCenter,
+          right: verticalCenter + tooltipWidth,
+        };
+      case 'bottom-start':
+        return {
+          top: triggerRect.bottom + gap,
+          bottom: triggerRect.bottom + gap + tooltipHeight,
+          left: triggerRect.left,
+          right: triggerRect.left + tooltipWidth,
+        };
+      case 'bottom-end':
+        return {
+          top: triggerRect.bottom + gap,
+          bottom: triggerRect.bottom + gap + tooltipHeight,
+          left: triggerRect.right - tooltipWidth,
+          right: triggerRect.right,
+        };
+      case 'left':
+        return {
+          top: horizontalCenter,
+          bottom: horizontalCenter + tooltipHeight,
+          left: triggerRect.left - tooltipWidth - gap,
+          right: triggerRect.left - gap,
+        };
+      case 'left-start':
+        return {
+          top: triggerRect.top,
+          bottom: triggerRect.top + tooltipHeight,
+          left: triggerRect.left - tooltipWidth - gap,
+          right: triggerRect.left - gap,
+        };
+      case 'left-end':
+        return {
+          top: triggerRect.bottom - tooltipHeight,
+          bottom: triggerRect.bottom,
+          left: triggerRect.left - tooltipWidth - gap,
+          right: triggerRect.left - gap,
+        };
+      case 'right':
+        return {
+          top: horizontalCenter,
+          bottom: horizontalCenter + tooltipHeight,
+          left: triggerRect.right + gap,
+          right: triggerRect.right + gap + tooltipWidth,
+        };
+      case 'right-start':
+        return {
+          top: triggerRect.top,
+          bottom: triggerRect.top + tooltipHeight,
+          left: triggerRect.right + gap,
+          right: triggerRect.right + gap + tooltipWidth,
+        };
+      case 'right-end':
+        return {
+          top: triggerRect.bottom - tooltipHeight,
+          bottom: triggerRect.bottom,
+          left: triggerRect.right + gap,
+          right: triggerRect.right + gap + tooltipWidth,
+        };
+      default:
+        return { top: 0, bottom: 0, left: 0, right: 0 };
+    }
+  }
+
+  useEffect(() => {
+    if (show) {
+      checkPlacement();
+      window.addEventListener('resize', checkPlacement);
+      return () => window.removeEventListener('resize', checkPlacement);
+    }
+  }, [show, placement]);
 
   useEffect(() => {
     if (trigger !== 'onClick') return;
@@ -32,7 +197,9 @@ export const Tooltip: FC<TooltipProps> = ({
     const handleClickOutside = (event: MouseEvent) => {
       if (
         tooltipRef.current &&
-        !tooltipRef.current.contains(event.target as Node)
+        !tooltipRef.current.contains(event.target as Node) &&
+        triggerRef.current &&
+        !triggerRef.current.contains(event.target as Node)
       ) {
         setShow(false);
       }
@@ -55,8 +222,9 @@ export const Tooltip: FC<TooltipProps> = ({
   };
 
   return (
-    <Box {...props} className={wrapper} ref={tooltipRef}>
+    <Box {...props} className={wrapper}>
       <Box
+        ref={triggerRef}
         onMouseEnter={trigger === 'onHover' ? handleMouseEnter : undefined}
         onMouseLeave={trigger === 'onHover' ? handleMouseLeave : undefined}
         onClick={trigger === 'onClick' ? handleClick : undefined}
@@ -65,9 +233,26 @@ export const Tooltip: FC<TooltipProps> = ({
       </Box>
 
       {show && (
-        <Box className={tooltipContent}>
-          {title && <Text as="p" textStyle={"body-md"} bold color={{base:'slate.0', _dark: 'slate.90'}}>{title}</Text>}
-          {text && <Text as="span" textStyle={"body-sm"} color={{base:'slate.0', _dark: 'slate.90'}}>{text}</Text>}
+        <Box className={tooltipContent} ref={tooltipRef}>
+          {title && (
+            <Text
+              as="p"
+              textStyle="body-md"
+              bold
+              color={{ base: 'slate.0', _dark: 'slate.90' }}
+            >
+              {title}
+            </Text>
+          )}
+          {text && (
+            <Text
+              as="span"
+              textStyle="body-sm"
+              color={{ base: 'slate.0', _dark: 'slate.90' }}
+            >
+              {text}
+            </Text>
+          )}
         </Box>
       )}
     </Box>
