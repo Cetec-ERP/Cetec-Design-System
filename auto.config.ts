e446473f68004dbb05c0e0669de1868c21a3059e
Ethan Wiggins <ethanwiggins@ethans-air.lan>
2025-09-25 15:20:50 -0500

    feat:auto setup

---
+++ auto.config.ts
@@ -0,0 +1,23 @@
+import { AutoRc } from 'auto';
+
+import { INpmConfig } from '@auto-it/npm';
+
+const npmOptions: INpmConfig = {
+  exact: true,
+  canaryScope: '@auto-canary',
+};
+
+/** Auto configuration */
+export default function rc(): AutoRc {
+  return {
+    plugins: ['released', ['npm', npmOptions]],
+    onlyPublishWithReleaseLabel: true,
+    baseBranch: 'main',
+    noVersionPrefix: true,
+    release: {
+      prerelease: true,
+    },
+    versionBranches: false,
+    prereleaseBranches: ['develop'],
+  };
+}
