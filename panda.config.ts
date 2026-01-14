c4f4aeba73f5c5182fc9fc43eab2ab12790da239
Shaun Fox <shaunrfox@gmail.com>
2026-01-14 13:01:13 -0600

    Storybook is working now

--- panda.config.ts
+++ panda.config.ts
@@ -1,16 +1,51 @@
 import { defineConfig } from '@pandacss/dev';
 import { cetecPreset } from './cetec-preset';
 
+// Control whether the staticCss is included or not
+// because it's not needed for production and
+// generates a ton of CSS
+const isStatic = process.env.PANDA_STATIC === 'true';
+
+const staticCss = isStatic
+  ? {
+      css: [
+        {
+          properties: {
+            background: ['*'],
+            color: ['*'],
+            border: ['*'],
+            fill: ['*'],
+            boxShadow: ['*'],
+            width: ['*'],
+            height: ['*'],
+            minWidth: ['*'],
+            minHeight: ['*'],
+            maxWidth: ['*'],
+            maxHeight: ['*'],
+            borderRadius: ['*'],
+            textStyle: ['*'],
+            fontFamily: ['*'],
+            fontSize: ['*'],
+            fontWeight: ['*'],
+            lineHeight: ['*'],
+            letterSpacing: ['*'],
+          },
+          conditions: ['light', 'dark'],
+        },
+      ],
+    }
+  : { css: [] };
+
 export default defineConfig({
   eject: true,
   gitignore: true,
   jsxFramework: 'react',
   jsxStyleProps: 'all',
   jsxFactory: 'styled',
-  preflight: false,
+  preflight: false, // do not add Panda's default reset styles
   strictTokens: true,
   watch: true,
-  clean: true,
+  clean: true, // empty /styled-system each run
 
   presets: [cetecPreset],
 
@@ -26,4 +61,8 @@ export default defineConfig({
   prefix: 'cetec',
   importMap: '@styled-system',
   outdir: 'styled-system',
+
+  staticCss: {
+    css: staticCss.css,
+  },
 });
